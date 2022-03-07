5d7f180a2b2f0abb8e5e7ad287c6416334d69f4c
Nicholas Nolen <nicnolen@ymail.com>
2022-03-07 11:53:46 -0600

    set up pizza routes in pizza-routes.js and prefixed and packaged them in the index.js file inside the api folder

---
+++ routes/api/index.js
@@ -0,0 +1,10 @@
+/* IMPORT API ROUTES TO PREFIX THEIR ENDPOINT NAMES AND PACKAGE THEM */
+// Import dependencies
+const router = require('express').Router;
+const pizzaRoutes = require('./pizza-routes');
+
+// Add prefix of `/pizzas` to routes created in `pizza-routes.js`
+router.use('pizzas', pizzaRoutes);
+
+// Export the router
+module.exports = router;
