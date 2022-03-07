4d00dd905c1b86ecbfeb24e889857c24043a92a2
Nicholas Nolen <nicnolen@ymail.com>
2022-03-07 13:36:37 -0600

    connected Comment model with Pizza model, added virtuals that reference the Comment model to the Pizza model, and imported the Comment model to the index.js file in the models folder

--- models/Pizza.js
+++ models/Pizza.js
@@ -3,27 +3,42 @@
 const { Schema, model } = require('mongoose');
 
 // Create the schema using the Schema constructor imported from Mongoose
-const PizzaSchema = new Schema({
-  pizzaName: {
-    type: String,
-  },
-  createdBy: {
-    type: String,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now, // If no value is provided in `createdAt`, this function will execute and provide a timestamp
-  },
-  size: {
-    type: String,
-    default: 'Large',
-  },
-  toppings: [], // Could have specified `Array` instead
-  comments: [
-    {
-      type: String, // Placeholder
+const PizzaSchema = new Schema(
+  {
+    pizzaName: {
+      type: String,
     },
-  ],
+    createdBy: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now, // if no value is provided in `createdAt`, this function will execute and provide a timestamp
+    },
+    size: {
+      type: String,
+      default: 'Large',
+    },
+    toppings: [], // could have specified `Array` instead
+    comments: [
+      {
+        type: Schema.Types.ObjectId, // tell Mongoose to expect an `ObjectId` and that the data comes from the Comment model
+        ref: 'Comment', // tells the Pizza model which documents to search for the right comments
+      },
+    ],
+  },
+  {
+    // tell the schema it can use virtuals
+    toJSON: {
+      virtuals: true,
+    },
+    id: false, // id is a virutal Mongoose returns and we dont need it
+  }
+);
+
+// Get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function () {
+  return this.comments.length;
 });
 
 // Create the Pizza model using PizzaSchema
