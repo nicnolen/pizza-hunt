4f36e311eace497769e1d26cbf011579eab0e378
Nicholas Nolen <nicnolen@ymail.com>
2022-03-07 13:44:31 -0600

    set up skeleton for the Comment controller

---
+++ controllers/comment-controller.js
@@ -0,0 +1,15 @@
+/* COMMENT MODEL FUNCTIONALITY */
+// Import dependencies
+const { Comment, Pizza } = require('../models');
+
+// Create functionality for the Comment model
+const commentController = {
+  // add comment to pizza
+  addComment() {},
+
+  // remove comment
+  removeComment() {},
+};
+
+// Export modules
+module.exports = commentController;
