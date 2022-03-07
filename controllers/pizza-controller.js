8f463bc011f6ca379a39c52a6d89eb56bbf04a0d
Nicholas Nolen <nicnolen@ymail.com>
2022-03-07 11:08:51 -0600

    created file and directory for the pizzaController object

---
+++ controllers/pizza-controller.js
@@ -0,0 +1,11 @@
+/* PIZZA MODEL FUNCTIONALITY */
+// Import the Pizza model
+const { Pizza } = require('../models');
+
+// Create the functionality for the Pizza model
+const pizzaController = {
+  // functions go here as methods
+};
+
+// Export the pizzaController
+module.exports = pizzaController;
