446f99108e95e2d01db7a17f9ed3eab8e1cf97d4
Nicholas Nolen <nicnolen@ymail.com>
2022-03-07 13:51:06 -0600

    created Pizza controller methods to add comments

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -5,7 +5,25 @@ const { Comment, Pizza } = require('../models');
 // Create functionality for the Comment model
 const commentController = {
   // add comment to pizza
-  addComment() {},
+  addComment({ params, body }, res) {
+    console.info(body);
+    Comment.create(body)
+      .then(({ _id }) => {
+        return Pizza.findOneAndUpdate(
+          { _id: params.pizzaId },
+          { $push: { comments: _id } }, // $push adds data to an array. All MongoDB functions start with a $
+          { new: true } // recieve back the updated pizza with the pizza comment
+        );
+      })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.json(err));
+  },
 
   // remove comment
   removeComment() {},
